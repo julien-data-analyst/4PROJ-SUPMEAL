d19b9b53d1103150b48313ab617c6c6ac9da9b68
julien-data-analyst <julienrenoult76460@gmail.com>
2026-07-25 19:44:38 +0200

    feat(backend): modified planning codes to fit with the latest updates from develop

--- backend/tests/planning/conftest.py
+++ backend/tests/planning/conftest.py
@@ -96,10 +96,12 @@ def cookbook_shared_with_regular_user(
     other_user: User,  # noqa: F811
     regular_user: User,  # noqa: F811
 ) -> Cookbook:
-    """A cookbook created by ``other_user`` and shared with ``regular_user``."""
+    """A cookbook created by ``other_user`` and shared with ``regular_user`` at
+    the "creator" role, i.e. with enough rights to schedule new plannings into it.
+    """
     cookbook = Cookbook(name="Carnet de Bob", creator=other_user)
     cookbook.save()
-    SharedUserCookbook(cookbook=cookbook, user=regular_user, role="editor").save()
+    SharedUserCookbook(cookbook=cookbook, user=regular_user, role="creator").save()
     return cookbook
 
 
